8699f73d2ec3cb750e99aa22746910b86468fa27
Gibraan Rahman <grahman@eng.ucsd.edu>
2020-01-30 10:41:24 -0800

    added more util functions & tests

--- rpca_cooc_benchmarking/python/tests/test_util.py
+++ rpca_cooc_benchmarking/python/tests/test_util.py
@@ -2,8 +2,18 @@
 
 import numpy as np
 import pandas as pd
+import pytest
 
 from rpca_cooc_benchmarking import util as rcb
+
+@pytest.fixture(scope="module")
+def example_adj_df():
+    F1 = ["V1", "V2", "V3", "V4"]
+    F2 = ["V2", "V3", "V1", "V1"]
+    df = pd.DataFrame([F1, F2]).T
+    df.columns = ["F1", "F2"]
+
+    return df
 
 def test_edge_equality():
     edge1 = rcb.Edge("A", "B")
@@ -21,18 +31,58 @@ def test_edge_inequality():
     assert edge1 != edge2
     assert edge1 != edge3
 
-def test_adj_loading():
-    F1 = ["A", "B", "C", "D"]
-    F2 = ["B", "C", "A", "A"]
-    df = pd.DataFrame([F1, F2]).T
-    df.columns = ["F1", "F2"]
-
-    loaded_edges = rcb.load_adj_list(df)
-
+def test_adj_loading(example_adj_df):
+    loaded_edges = rcb.load_adj_list(example_adj_df)
     truth_edges = [
-        rcb.Edge("A", "B"),
-        rcb.Edge("B", "C"),
-        rcb.Edge("C", "A"),
-        rcb.Edge("D", "A"),
+        rcb.Edge("V1", "V2"),
+        rcb.Edge("V2", "V3"),
+        rcb.Edge("V3", "V1"),
+        rcb.Edge("V4", "V1"),
     ]
     assert loaded_edges == truth_edges
+
+def test_reshape_mat():
+    mat = [
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+    ]
+
+    df = pd.DataFrame(mat)
+    reshaped_vector = rcb.reshape_mat(df)
+    target_vector = [0, 1, 0, 0, 1, 0]
+
+    assert reshaped_vector == target_vector
+
+def test_reshape_mat_non_symm():
+    mat = [
+        [0, 0, 1, 0],
+        [1, 0, 0, 1],
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+    ]
+
+    df = pd.DataFrame(mat)
+    with pytest.raises(ValueError):
+        rcb.reshape_mat(df)
+
+def test_adj_list_to_class(example_adj_df):
+    """
+    F1 = ["A", "B", "C", "D"]
+    F2 = ["B", "C", "A", "A"]
+
+     ---------
+    | F1 | F2 |
+    |----|----|      A B C D
+    | A  | B  |    A 0 1 1 1
+    | B  | C  | -> B 1 0 1 0
+    | C  | A  |    C 1 0 0 0
+    | D  | A  |    D 1 0 0 0
+     ---------
+
+    Target: [1, 1, 1, 0, 0, 0]
+    """
+    test_values = rcb.adj_list_to_class(example_adj_df, 4)
+    target_values = [1, 1, 1, 1, 0, 0]
+    assert test_values == target_values
--- rpca_cooc_benchmarking/python/util.py
+++ rpca_cooc_benchmarking/python/util.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import numpy as np
+
 class Edge:
 
     def __init__(self, node1: str, node2: str):
@@ -14,6 +16,40 @@ class Edge:
 
         return sort_edge1 == sort_edge2
 
+    def __str__(self):
+        return f"{self.node1}--{self.node2}"
+
+    def __repr__(self):
+        return(self.__str__())
+
 def load_adj_list(adj_df):
     edge_list = [Edge(row.F1, row.F2) for i, row in adj_df.iterrows()]
     return edge_list
+
+def reshape_mat(mat):
+    """Collapse symmetric matrix into upper triangular vector."""
+
+    # check for symmetry
+    if not np.allclose(mat, mat.T, rtol=1e-05, atol=1e-08):
+        raise(ValueError("Matrix is not symmetric!"))
+
+    n = mat.shape[0]
+
+    values = list()
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            values.append(mat.iloc[i][j])
+
+    return values
+
+def adj_list_to_class(adj_df, n):
+    """Convert list of edges to binary list for roc."""
+    values = list()
+    edge_list = load_adj_list(adj_df)
+
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            test_edge = Edge(f"V{i}", f"V{j}")
+            values.append(int(test_edge in edge_list))
+
+    return values
